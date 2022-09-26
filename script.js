62788d057c555ccf62d95439163f5eb682de2d77
Aslan <example@gmail.com>
2022-09-26 23:45:35 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+
+
+var number = 5;
+var string = 'hello';
+var symbol = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+let something;
+console.log(something);
+console.log();
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2); 
+console.log("2" == 2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
